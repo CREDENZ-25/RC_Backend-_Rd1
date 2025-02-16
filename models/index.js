700b70d2e755784f85ea0fdf49e50673c0df99f1
gayatrid-byte <gayatrida05@gmail.com>
2025-02-16 23:30:20 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,21 @@
 const Progress = require('./Progress');
 const UserModel = require('./User');
 
+
+const db = require('../config/db');
+
+
+
+// Define associations here
+UserModel.hasOne(Progress, { foreignKey: 'user_id', as: 'progress' });
+Progress.belongsTo(UserModel, { foreignKey: 'user_id', as: 'user' });
+
+// Export models
+module.exports = {
+  UserModel,
+  Progress,
+  db,
+};
+
 module.exports = { Progress, UserModel };
+
