387dbd1476b93249d57cad258dbb076bfe1bb867
aniketxkardile <kardileaniket706@gmail.com>
2025-02-19 23:19:44 +0530

    modify skip

--- controllers/skip.contoller.js
+++ controllers/skip.contoller.js
@@ -61,7 +61,7 @@ const skipController = async (req, res) => {
     const freezeStatus = freezell;
     const nextQuestionId = questionArray[counter];
     const nextQuestion = await Question.findOne({ where: { question_id: nextQuestionId }, attributes: { exclude: ['answer'] } });
-    return res.status(200).json({ message: "Skip lifeline used successfully", nextQuestion, timeLeft, doubleStatus, skipStatus, freezeStatus });
+    return res.status(200).json({ message: "Skip lifeline used successfully", nextQuestion, timeLeft, doubleStatus, skipStatus, freezeStatus, marks: marks });
 
   } catch (error) {
     console.log(error);
